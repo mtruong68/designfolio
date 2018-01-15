a2f8e2d3cf434e088b6734427a23edc9334c4436
mtruong68 <marytruong2020@u.northwestern.edu>
2018-01-15 02:50:14 -0600

    all

---
+++ index.js
@@ -0,0 +1,31 @@
+var ara = document.getElementById("ara");
+var soundshot = document.getElementById("soundshot");
+var intellishelter = document.getElementById("intellishelter");
+
+function imgreplace1(){
+	document.getElementById("background-img").src = "images/SSAd1.png";
+}
+
+function imgreplace2(){
+	document.getElementById("background-img").src = "images/Intellishelter.png";
+}
+
+function imgreplace3(){
+	document.getElementById("background-img").src = "images/Ara.png";
+}
+
+function imgreplace4(){
+	document.getElementById("background-img").src = "images/Github.png";
+}
+
+function imgreplace5(){
+	document.getElementById("background-img").src = "images/Website.png";
+}
+
+function imgreplace6(){
+	document.getElementById("background-img").src = "images/1.png";
+}
+
+function regimg(){
+	document.getElementById("background-img").src = "images/background2.png";
+}
